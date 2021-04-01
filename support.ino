40eaaa2397c35214a482ef9f17619cf2ea132560
akinnae <36315296+akinnae@users.noreply.github.com>
2021-03-31 21:25:55 -0700

    Create support.ino
    Contains support functions - delay & rainbow

---
+++ support.ino
@@ -0,0 +1,21 @@
+// Delay for i milliseconds since the last check
+void delayMillis(unsigned long int start, unsigned long int i){
+  unsigned long int currMillis = 0;
+  while((currMillis - start) < i){
+    currMillis = millis();
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t wheel(byte wheelPos) {
+  if(wheelPos < 85) {
+   return strip.Color(wheelPos * 3, 255 - wheelPos * 3, 0);
+  } else if(wheelPos < 170) {
+   wheelPos -= 85;
+   return strip.Color(255 - wheelPos * 3, 0, wheelPos * 3);
+  } else {
+   wheelPos -= 170;
+   return strip.Color(0, wheelPos * 3, 255 - wheelPos * 3);
+  }
+}
